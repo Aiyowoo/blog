9805986eadfb143c21a967b385d294a5783762d9
moon <m879224490@qq.com>
2017-08-21 17:36:10 +0800

    完成log部分的配置

--- app/email.py
+++ app/email.py
@@ -6,7 +6,7 @@ mail = Mail()
 
 
 def sendMail(reciever, subject, contentTemplate, **kwargs):
-    message = Message(current_app['BLOG_MAIL_SUBJECT_PREFIX'] + subject,
+    message = Message(current_app.config['BLOG_MAIL_SUBJECT_PREFIX'] + subject,
                       sender=current_app.config['BLOG_MAIL_SENDER'],
                       recipients=[reciever])
     message.html = render_template(contentTemplate, **kwargs)
